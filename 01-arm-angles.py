cebd2b63aee39274a5f7a8b9d631e61ecdea5146
Jack Chen <jackchen@matterhorn.ece.pdx.edu>
2017-10-10 12:47:39 -0700

    Fixed law of sines input

--- 01-arm-angles.py
+++ 01-arm-angles.py
@@ -1,10 +1,11 @@
 #TODO Convert numpy to math library
 import math
-import numpy as np
+#import numpy as np
 import pickle
 
+# These should be equal
 LENGTH_FOREARM = 9
-LENGTH_BICEP = 12
+LENGTH_BICEP = 9
 
 def cosines_law_(A, B, C):
     #return np.degrees(np.arccos(((C**2 - A**2 - B**2)/(-2 * A * B))))
@@ -24,7 +25,7 @@ def cart_to_angles(x, y, l1=LENGTH_FOREARM, l2=LENGTH_BICEP):
     # Remember to convert radians = degrees, np.degrees
     x = int(x)
     y = int(y)
-    r = np.sqrt(x ** 2 + y ** 2)
+    r = math.sqrt(x ** 2 + y ** 2)
     # theta1 is the angle bound by origin
     # theta2 is angle found near the cartesian point
     theta1 = math.asin(y/r)
@@ -38,9 +39,9 @@ def cart_to_angles(x, y, l1=LENGTH_FOREARM, l2=LENGTH_BICEP):
     # Law of Sines: sin(a)/A = sin(b)/B = sin(c)/C
     # sin(c)/C = c_, Rewritten: b = arcsin(Bc_), a = arcsin(Ac_)
     #TODO: check if below works with ALL cartesian points => theta1
-    shoulder_angle = sines_law_(elbow_angle, l1) + theta1
+    shoulder_angle = sines_law_(math.sin(math.radians(elbow_angle))/r, l1) + theta1
 
     return elbow_angle, shoulder_angle
 
 if __name__ == '__main__':
-    cart_to_angles()
+    print(cart_to_angles())
