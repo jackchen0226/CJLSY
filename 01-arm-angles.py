f81802dd5d9c4d6bd7a386b94cd7519aab17f381
Jack Chen <jackchen010@yahoo.com>
2017-10-23 22:23:32 -0700

    Redone shoulder angle calculation with isoceles trig

--- 01-arm-angles.py
+++ 01-arm-angles.py
@@ -8,13 +8,19 @@ LENGTH_FOREARM = 9
 LENGTH_BICEP = 9
 
 def cosines_law_(A, B, C):
-    #return np.degrees(np.arccos(((C**2 - A**2 - B**2)/(-2 * A * B))))
-    return math.degrees(math.acos((C**2 - A**2 - B**2)/(-2 * A * B)))
+    #return np.degrees(np.arccos(((C**2 - A**2 - B**2)/(2 * A * B))))
+    n = float(C**2) - (A**2) - (B**2)
+    d = float(2 * A * B)
+    print(n, d)
+    return math.acos(n / d)
 
-def sines_law_(c_, length):
+def sines_law_(C, c, length):
     #return np.degrees(np.arcsin(length * c_))
-    return math.degrees(math.asin(length * c_))
+    return math.asin(length * (math.sin(c)/C))
 
+def isoceles_angle(arm_length, base):
+    height = math.sqrt((arm_length**2) - ((base/2.0)**2))
+    return math.atan(2*height/base)
 import click
 
 @click.command()
@@ -25,24 +31,42 @@ def cart_to_angles(x, y, l1=LENGTH_FOREARM, l2=LENGTH_BICEP):
     # Remember to convert radians = degrees, np.degrees
     x = int(x)
     y = int(y)
-    r = math.sqrt(x ** 2 + y ** 2)
+    r = math.sqrt((x ** 2) + (y ** 2))
     # theta1 is the angle bound by origin
     # theta2 is angle found near the cartesian point
-    theta1 = math.asin(y/r)
+    theta1 = math.degrees(math.asin(y/r))
     theta2 = math.asin(x/r)
-
+    print(theta1)
     # Law of Cosines: C ** 2 = A ** 2 + B ** 2 - 2ABcos(c)
-    # Rewritten: arccos((C ** 2 - A ** 2 - B ** 2)/(-2 * A * B))
+    # Rewritten: arccos((C ** 2 - A ** 2 - B ** 2)/(2 * A * B))
     
-    elbow_angle = cosines_law_(l1, l2, r)
-
+    elbow_angle = math.degrees(cosines_law_(l1, l2, r))
     # Law of Sines: sin(a)/A = sin(b)/B = sin(c)/C
     # sin(c)/C = c_, Rewritten: b = arcsin(Bc_), a = arcsin(Ac_)
     #TODO: check if below works with ALL cartesian points => theta1
-    shoulder_angle = sines_law_(math.sin(math.radians(elbow_angle))/r, l1) + theta1
-    print(elbow_angle)
-    print(shoulder_angle)
-    return elbow_angle, shoulder_angle
+    shoulder_angle = math.degrees(isoceles_angle(l1, r))
+    elbow_angle = 180. - 2*shoulder_angle
+    if x < 0 and y > 0:
+        print('thr')
+        # Quadrant 2
+        theta1 += 90.
+    elif x < 0 and y < 0:
+        # Quadrant 3
+        theta1 = -theta1
+        theta1 += 180.
+        print(theta1)
+    elif x > 0 and y < 0:
+        # Quadrant 4
+        theta1 = -theta1
+        theta1 += 270.
+
+    shoulder_angle += theta1
+    if shoulder_angle > 360.:
+        shoulder_angle -= 360.
+
+    print("Shoulder angle: {}, Elbow angle: {}".format(str(shoulder_angle), str(elbow_angle)))
+    
 
 if __name__ == '__main__':
-    print(cart_to_angles())
+    cart_to_angles()
+
